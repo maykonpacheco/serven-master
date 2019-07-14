a0ac86baa4299ce823d91513911d043adf55b303
Maykon Pacheco <maykonpachecogeo@gmail.com>
2019-07-14 00:26:51 -0400

    pagina de servicos terminada

--- src/Components/Pages/Servicos/index.js
+++ src/Components/Pages/Servicos/index.js
@@ -4,6 +4,8 @@ import firebase from "../../../firebase";
 import "./styles.css";
 import Navbar from "../../Navbar";
 
+
+
 import { Container, Row, Button, Card } from "react-bootstrap";
 
 class Servicos extends Component {
@@ -38,16 +40,22 @@ class Servicos extends Component {
   onCollectionUpdate = querySnapshot => {
     const Especialist = [];
     querySnapshot.forEach(doc => {
-      const { nome, hour, domingo } = doc.data();
+      const { nome, segunda, domingo, terca, quarta, quinta, sexta, sabado } = doc.data();
       Especialist.push({
         key: doc.id,
         doc, // DocumentSnapshot
         nome,
         domingo,
-        hour
+        segunda,
+        terca,
+        quarta,
+        quinta,
+        sexta,
+        sabado
       });
       console.log(doc.id, " => ", doc.data());
     });
+    console.log(Especialist)
     this.setState({
       Especialist
     });
@@ -63,7 +71,7 @@ class Servicos extends Component {
         <Navbar />
         <Container>
           <div className="centerService">
-            <h5 className="fontColor col-12 col-sm-12 col-lg-12">
+            <h5 className="fontData fontColor col-12 col-sm-12 col-lg-12">
               Domingo, 12 de Abril
             </h5>
             {this.state.Especialist.map(board => (
@@ -72,20 +80,202 @@ class Servicos extends Component {
                   <div className="card-header">
                     <h4 className="fontColor">{board.nome}</h4>
                   </div>
-                  {board.domingo.map(board => (
-                    <div className="card-header left-button">
+                    <div className="card-header row">
                       <div class="col mb-4 mb-lg-0 text-center">
                         <div class="row">
-                         {
+                        {board.domingo.map(board => (
+                          <div className="button-hour">
+                            { board.value === true ?  
                           <button type="submit" className="btn btn-primary">
-                            {board.hour}
+                            {board.hour} 
                           </button>
-                         } 
-                          {console.log("aqui", board.value)}
+                          : null
+                            } 
+                          </div>
+                         ))}
                         </div>
                       </div>
                     </div>
-                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="centerService">
+            <h5 className="fontColor fontData col-12 col-sm-12 col-lg-12">
+              Segunda, 13 de Abril
+            </h5>
+            {this.state.Especialist.map(board => (
+              <div className="col-12 col-sm-12 col-lg-12">
+                <div class="card">
+                  <div className="card-header">
+                    <h4 className="fontColor">{board.nome}</h4>
+                  </div>
+                    <div className="card-header row">
+                      <div class="col mb-4 mb-lg-0 text-center">
+                        <div class="row">
+                        {board.segunda.map(board => (
+                          <div className="button-hour">
+                            { board.value === true ?  
+                          <button type="submit" className="btn btn-primary">
+                            {board.hour} 
+                          </button>
+                          : null
+                            } 
+                          </div>
+                         ))}
+                        </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="centerService">
+            <h5 className="fontColor fontData col-12 col-sm-12 col-lg-12">
+              Terça, 14 de Abril
+            </h5>
+            {this.state.Especialist.map(board => (
+              <div className="col-12 col-sm-12 col-lg-12">
+                <div class="card">
+                  <div className="card-header">
+                    <h4 className="fontColor">{board.nome}</h4>
+                  </div>
+                    <div className="card-header row">
+                      <div class="col mb-4 mb-lg-0 text-center">
+                        <div class="row">
+                        {board.terca.map(board => (
+                          <div className="button-hour">
+                            { board.value === true ?  
+                          <button type="submit" className="btn btn-primary">
+                            {board.hour} 
+                          </button>
+                          : null
+                            } 
+                          </div>
+                         ))}
+                        </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="centerService">
+            <h5 className="fontColor fontData col-12 col-sm-12 col-lg-12">
+              Quarta, 15 de Abril
+            </h5>
+            {this.state.Especialist.map(board => (
+              <div className="col-12 col-sm-12 col-lg-12">
+                <div class="card">
+                  <div className="card-header">
+                    <h4 className="fontColor">{board.nome}</h4>
+                  </div>
+                    <div className="card-header row">
+                      <div class="col mb-4 mb-lg-0 text-center">
+                        <div class="row">
+                        {board.quarta.map(board => (
+                          <div className="button-hour">
+                            { board.value === true ?  
+                          <button type="submit" className="btn btn-primary">
+                            {board.hour} 
+                          </button>
+                          : null
+                            } 
+                          </div>
+                         ))}
+                        </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="centerService">
+            <h5 className="fontColor fontData col-12 col-sm-12 col-lg-12">
+              Quinta, 16 de Abril
+            </h5>
+            {this.state.Especialist.map(board => (
+              <div className="col-12 col-sm-12 col-lg-12">
+                <div class="card">
+                  <div className="card-header">
+                    <h4 className="fontColor">{board.nome}</h4>
+                  </div>
+                    <div className="card-header row">
+                      <div class="col mb-4 mb-lg-0 text-center">
+                        <div class="row">
+                        {board.quinta.map(board => (
+                          <div className="button-hour">
+                            { board.value === true ?  
+                          <button type="submit" className="btn btn-primary">
+                            {board.hour} 
+                          </button>
+                          : null
+                            } 
+                          </div>
+                         ))}
+                        </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="centerService">
+            <h5 className="fontColor fontData col-12 col-sm-12 col-lg-12">
+              Sexta, 17 de Abril
+            </h5>
+            {this.state.Especialist.map(board => (
+              <div className="col-12 col-sm-12 col-lg-12">
+                <div class="card">
+                  <div className="card-header">
+                    <h4 className="fontColor">{board.nome}</h4>
+                  </div>
+                    <div className="card-header row">
+                      <div class="col mb-4 mb-lg-0 text-center">
+                        <div class="row">
+                        {board.sexta.map(board => (
+                          <div className="button-hour">
+                            { board.value === true ?  
+                          <button type="submit" className="btn btn-primary">
+                            {board.hour} 
+                          </button>
+                          : null
+                            } 
+                          </div>
+                         ))}
+                        </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="centerService">
+            <h5 className="fontColor fontData col-12 col-sm-12 col-lg-12">
+              Sabado, 18 de Abril
+            </h5>
+            {this.state.Especialist.map(board => (
+              <div className="col-12 col-sm-12 col-lg-12">
+                <div class="card">
+                  <div className="card-header">
+                    <h4 className="fontColor">{board.nome}</h4>
+                  </div>
+                    <div className="card-header row">
+                      <div class="col mb-4 mb-lg-0 text-center">
+                        <div class="row">
+                        {board.sabado.map(board => (
+                          <div className="button-hour">
+                            { board.value === true ?  
+                          <button type="submit" className="btn btn-primary">
+                            {board.hour} 
+                          </button>
+                          : null
+                            } 
+                          </div>
+                         ))}
+                        </div>
+                      </div>
+                    </div>
                 </div>
               </div>
             ))}
